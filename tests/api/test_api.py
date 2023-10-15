19d5fab4b555ef540c1de93290633418f7733799
Alex Morozov <al_79@icloud.com>
2023-10-15 18:28:34 +0300

    Changes

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,27 +1,18 @@
 import pytest
 
 
-class User:
-
-    def __init__(self) -> None:
-        self.name = 'Alex'
-        self.second_name = 'Morozov'
-
-
-@pytest.fixture
-def user():
-   yield User()
-
-
+@pytest.mark.change
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
 
 
+@pytest.mark.check
 def test_name(user):
     assert user.name == 'Alex'
 
 
+@pytest.mark.check
 def test_second_name(user):
     assert user.second_name == 'Morozov'
     
